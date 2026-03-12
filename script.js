f43910f0231754d9977a92b08fa42dac2612cb9c
Danial-codez <danialzia2004@gmail.com>
2026-03-13 01:37:11 +0500

    added the play functionality

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 console.log("Lets start JS");
 
 function formatTime(seconds) {
-  // Ensure it's a valid number
   seconds = Number(seconds);
-
-  // Convert to whole seconds (removes decimals like 20.7483)
   seconds = Math.floor(seconds);
-
   const mins = Math.floor(seconds / 60);
   const secs = seconds % 60;
-
   return String(mins).padStart(2, "0") + ":" + String(secs).padStart(2, "0");
 }
 
@@ -26,43 +21,32 @@ const playmusic = (track, showpause = false) => {
 };
 
 async function getSongs() {
-  let a = await fetch("http://127.0.0.1:5500/songs/");
-  let response = await a.text();
-  let div = document.createElement("div");
-  div.innerHTML = response;
-  let as = div.getElementsByTagName("a");
-  let songs = [];
-  for (let index = 0; index < as.length; index++) {
-    const element = as[index];
-    if (element.href.endsWith(".mp3")) {
-      songs.push(element.href.split("/songs/")[1]);
-    }
-  }
+  let response = await fetch("/songs/songs.json");
+  let songs = await response.json();
   return songs;
 }
 
 async function main() {
   let songs = await getSongs();
   playmusic(songs[0], true);
-  let songUL = document
-    .querySelector(".songlist")
-    .getElementsByTagName("ul")[0];
+
+  let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0];
+
   for (const song of songs) {
     songUL.innerHTML += `<li>
-                <img class="invert" src="music.svg" alt="music">
-                  <div class="info">
-                    <div>${song.replaceAll("%20", " ")}</div>
-                    <div>Dani</div>
-                  </div>
-                <img class="invert" src="playbarplay.svg" alt="">
-               </li>`;
+      <img class="invert" src="music.svg" alt="music">
+      <div class="info">
+        <div>${song.replaceAll("%20", " ")}</div>
+        <div>Artist</div>
+      </div>
+      <img class="invert" src="playbarplay.svg" alt="">
+    </li>`;
   }
 
   Array.from(
-    document.querySelector(".songlist").getElementsByTagName("li"),
+    document.querySelector(".songlist").getElementsByTagName("li")
   ).forEach((e) => {
-    e.addEventListener("click", (element) => {
-      console.log(e.querySelector(".info").firstElementChild.innerHTML);
+    e.addEventListener("click", () => {
       playmusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
     });
   });
@@ -79,9 +63,31 @@ async function main() {
 
   currentsong.addEventListener("timeupdate", () => {
     document.querySelector(".songtime").innerHTML =
-      `${formatTime(currentsong.currentTime)}:${formatTime(currentsong.duration)}`;
+      `${formatTime(currentsong.currentTime)} / ${formatTime(currentsong.duration)}`;
     document.querySelector(".circle").style.left =
       (currentsong.currentTime / currentsong.duration) * 100 + "%";
+  });
+
+  // Click on seekbar to seek
+  document.querySelector(".seekbar").addEventListener("click", (e) => {
+    let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+    document.querySelector(".circle").style.left = percent + "%";
+    currentsong.currentTime = (currentsong.duration * percent) / 100;
+  });
+
+  // Next and Previous buttons
+  next.addEventListener("click", () => {
+    let index = songs.indexOf(currentsong.src.split("/songs/")[1]);
+    if (index + 1 < songs.length) {
+      playmusic(songs[index + 1]);
+    }
+  });
+
+  prev.addEventListener("click", () => {
+    let index = songs.indexOf(currentsong.src.split("/songs/")[1]);
+    if (index - 1 >= 0) {
+      playmusic(songs[index - 1]);
+    }
   });
 }
 
